dae84b7e670b94e36e2a52f8577ee0b42c58f1dc
cpmachado <cpmachado@protonmail.com>
2022-10-14 19:10:32 +0100

    Fix flake8 lint errors in simple

--- src/selectk/simple.py
+++ src/selectk/simple.py
@@ -4,6 +4,11 @@ Use the simplest approach to solve the problem
 
 from typing import List
 
-def find_kth_largest_ele(nums: List[int], k: int ) -> int:
-    nums.sort() #sort the array
-    return nums[len(nums) - k] #count down from the righ most part of the array to find the 'kth largest element'
+
+def find_kth_largest_ele(nums: List[int], k: int) -> int:
+    # sort the array
+    nums.sort()
+
+    # count down from the righ most part of the array
+    # to find the 'kth largest element'
+    return nums[len(nums) - k]
